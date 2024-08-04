e8523b7ad46014fa97fe798ffed90a507b65e43c
Harish Thampi <harishbhupeshthampi@gmail.com>
2024-08-03 22:17:07 -0400

    Commit

--- src/auth/Auth0ProviderWithNavigate.tsx
+++ src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { AppState, Auth0Provider, User} from "@auth0/auth0-react";
+import {  Auth0Provider} from "@auth0/auth0-react";
 //Auth0Provider wraps your application and handles the authentication logic.
 import { useNavigate } from "react-router-dom";
 
